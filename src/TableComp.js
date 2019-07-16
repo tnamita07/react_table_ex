ea86ceb8646b7b2f362898156ceff574e4d70fb6
tnamita <tnamita07@gmail.com>
2019-07-16 18:17:55 +0530

    Initial commit

---
+++ src/TableComp.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+
+class TableComp extends React.Component {
+  
+    render() {
+      const data = [{
+        name: 'Roy Agasthyan',
+        age: 26
+      },{
+        name: 'Sam Thomason',
+        age: 22
+      },{
+        name: 'Michael Jackson',
+        age: 36
+      },{
+        name: 'Samuel Roy',
+        age: 56
+      },{
+        name: 'Rima Soy',
+        age: 28
+      },{
+        name: 'Suzi Eliamma',
+        age: 28
+      }]
+  
+      const columns = [{
+        Header: 'Name',
+        accessor: 'name'
+      },{
+        Header: 'Age',
+        accessor: 'age'
+      }]
+  
+      return (
+            <div>
+                <ReactTable
+                  data={data}
+                  columns={columns}
+                  defaultPageSize = {3}
+                  pageSizeOptions = {[3, 6]}
+                />
+            </div>      
+      )
+  
+    }
+  }
+  
+  export default TableComp;
+
